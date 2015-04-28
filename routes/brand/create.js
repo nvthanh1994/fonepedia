122cb5378f6fb12633ba83dab449c0be1d2dfbb7
nvthanh1994 <nvthanh1994@gmail.com>
2015-04-28 22:46:00 +0700

    Update brand API

--- routes/brand/create.js
+++ routes/brand/create.js
@@ -2,6 +2,8 @@ module.exports = function (req, res) {
     var connection = require('./../../config/database').connection;
     var brand_name = req.body.brand_name;
     var brand_id = req.body.brand_id;
+    var logoUrl = req.body.logoUrl;
+    var slogan = req.body.slogan;
     connection.query(
         'SELECT * FROM Brand WHERE brand_id="' + brand_id + '"',
         function (err, rows, fields) {
@@ -14,7 +16,7 @@ module.exports = function (req, res) {
                 return;
             }
             connection.query(
-                'INSERT INTO Brand (brand_name, brand_id,  numberOfPhone) VALUES ("' + brand_name + '","' + brand_id + '","' + 0 + '")',
+                'INSERT INTO Brand (brand_name, brand_id, numberOfPhone, logoUrl, slogan) VALUES ("' + brand_name + '","' + brand_id + '","' + 0 + '","' + logoUrl + '","' + slogan +'")',
                 function (err, rows, fields) {
                     console.log(err);
                     res.json({error_code: 0});
